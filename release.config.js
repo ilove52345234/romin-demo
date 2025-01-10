1ed3b90ef06b1493eb1e7ebbe110f448267b9161
romin.zhang <romin.zhang@104.com.tw>
2025-01-10 18:39:12 +0800

    fix: 增加環境別

--- release.config.js
+++ release.config.js
@@ -8,7 +8,7 @@ module.exports = {
         {
             name: 'staging',
             channel: 'staging',
-            // tagFormat: 'staging/v${nextRelease.version}'
+            tagFormat: 'staging/v${version}'
         },
         {
             name: 'master',
