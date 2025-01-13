e717dd5b4eb8cb30a2a94bf7eca3322545f0a5b2
romin.zhang <romin.zhang@104.com.tw>
2025-01-13 18:33:16 +0800

    fix: 測試10

--- release.config.js
+++ release.config.js
@@ -8,7 +8,7 @@ module.exports = {
         {
             name: 'staging',
             channel: 'staging',
-            tagFormat: 'staging/v${version}'
+            // tagFormat: 'staging/v${version}'
         },
         {
             name: 'master',
