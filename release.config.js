b5f4c7b0648ba84aa74caea91f7b917e639420f8
romin.zhang <romin.zhang@104.com.tw>
2025-01-14 18:05:01 +0800

    fix: 增加發布

--- release.config.js
+++ release.config.js
@@ -10,15 +10,16 @@ module.exports = {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
+        "./generate-pr-notes.js",
         '@semantic-release/changelog',
-        // '@semantic-release/github',
+        '@semantic-release/github',
         '@semantic-release/git',
-        {
-            "path": "@semantic-release/github",
-            "assets": ["dist/**"],
-            "releaseNotes": {
-                "template": "### Merged PRs\n{{#each merged_prs}}{{this}}\n{{/each}}"
-            }
-        }
+        // {
+        //     "path": "@semantic-release/github",
+        //     "assets": ["dist/**"],
+        //     "releaseNotes": {
+        //         "template": "### Merged PRs\n{{#each merged_prs}}{{this}}\n{{/each}}"
+        //     }
+        // }
     ]
 };
