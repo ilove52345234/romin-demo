6f5a4226228a8a3a2a1ddd0050a77f4ab653707e
romin.zhang <romin.zhang@104.com.tw>
2025-01-14 11:25:39 +0800

    feat: 初版架構

--- release.config.js
+++ release.config.js
@@ -2,28 +2,16 @@ module.exports = {
     branches: [
         {
             name: 'lab',
-            channel: 'lab',
-            tagFormat: 'lab/v${version}'
+            channel: 'lab'
         },
-        {
-            name: 'staging',
-            channel: 'staging',
-            tagFormat: 'staging/v${version}'
-        },
-        {
-            name: 'master',
-            channel: 'prod',
-            tagFormat: 'prod/v${version}'
-        },
+
     ],
+    tagFormat: "lab/v${version}",
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
         '@semantic-release/changelog',
         '@semantic-release/github',
-        {
-            "name": "@semantic-release/git",
-            "tagFormat": "${version}"
-        }
+        '@semantic-release/git',
     ]
 };
