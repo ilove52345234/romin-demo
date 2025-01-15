d726d87e2d398d20ba3c077bdf0ca15d9791d31e
romin.zhang <romin.zhang@104.com.tw>
2025-01-15 18:16:52 +0800

    簡單處理

--- release.config.js
+++ release.config.js
@@ -1,36 +1,10 @@
 
 const customTransform = (commit, context) => {
 
-
     // 檢查提交訊息是否包含 'Merge'
     if (commit.message && commit.message.includes('Merge pull request')) {
         commit.type = '🔀 Merge Commits';  // 設置為合併提交
-        return commit;
     }
-
-    // 處理其他提交類型的邏輯
-    if (commit.type === 'feat') {
-        commit.type = '✨ Features';
-    } else if (commit.type === 'fix') {
-        commit.type = '🐞 Bug Fixes';
-    } else if (commit.type === 'perf') {
-        commit.type = '🎈 Performance Improvements';
-    } else if (commit.type === 'revert') {
-        commit.type = 'Reverts';
-    } else if (commit.type === 'docs') {
-        commit.type = '📃 Documentation';
-    } else if (commit.type === 'style') {
-        commit.type = '🌈 Styles';
-    } else if (commit.type === 'refactor') {
-        commit.type = '🦄 Code Refactoring';
-    } else if (commit.type === 'test') {
-        commit.type = '🧪 Tests';
-    } else if (commit.type === 'build') {
-        commit.type = '🔧 Build System';
-    } else if (commit.type === 'ci') {
-        commit.type = '🐎 Continuous Integration';
-    }
-
     return commit;
 };
 
