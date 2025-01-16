2e1b0c0a4a211d223ccdfc3882bbb26d49976ba9
romin.zhang <romin.zhang@104.com.tw>
2025-01-16 14:49:09 +0800

    修改提交

--- release.config.js
+++ release.config.js
@@ -20,18 +20,49 @@ module.exports = {
     ],
     writerOpts: { transform: customTransform },
     tagFormat: "prod/v${version}",
+    releaseRules: [{ "type": "chore", "release": "patch" }],
+
     plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                "preset": "conventionalCommits",
+                "releaseRules": [
+                    {
+                        "type": "merge",
+                        "section": "Merged Pull Requests",
+                        "header": "Merge pull request"  // 用正則表達式匹配包含 "Merge pull request" 的提交
+                    },
+                    {
+                        "type": "pr",  // 自定義PR類型的提交規則
+                        "section": "Pull Request Updates",
+                        "header": "Merge branch"
+                    }
+                ]
+            }
+        ],
+        [
+            "@semantic-release/release-notes-generator",
+            {
+                "preset": "conventionalCommits",
+                "writerOpts": {
+                    "types": [
+                        {
+                            "type": "merge",
+                            "section": "Merged Pull Requests"
+                        },
+                        {
+                            "type": "pr",
+                            "section": "Pull Request Updates"
+                        }
+                    ]
+                }
+            }
+        ],
+        // "@semantic-release/commit-analyzer",
+        // '@semantic-release/release-notes-generator',
         '@semantic-release/changelog',
         '@semantic-release/github',
         '@semantic-release/git',
-        // {
-        //     "path": "@semantic-release/github",
-        //     "assets": ["dist/**"],
-        //     "releaseNotes": {
-        //         "template": "### Merged PRs\n{{#each merged_prs}}{{this}}\n{{/each}}"
-        //     }
-        // }
     ]
 };
